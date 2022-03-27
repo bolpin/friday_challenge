be1cda31deabbc230b5bd670bed13b3273b3ed5d
Brian Olpin <bolpin@gmail.com>
2022-03-27 12:09:58 -0700

    age validation and player tests added

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,19 @@ class Player < ApplicationRecord
   has_one :device, dependent: :destroy
   belongs_to :gender, optional: true
 
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :birthdate, presence: true
+
+  validate :minimum_fourteen_years_old
+
+  # TODO: set a database-level constraint on this as well
+  # because of liability
+  def minimum_fourteen_years_old
+    if self.birthdate && self.birthdate > 14.years.ago.to_date
+      errors.add(:birthdate, "player too young")
+    end
+  end
+
   accepts_nested_attributes_for :device
 end
