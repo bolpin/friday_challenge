1fd34b4617c23a822996c0c4efdafbe1f7473fc5
Brian Olpin <bolpin@gmail.com>
2022-03-30 11:06:21 -0700

    added esbuild and react

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'components/index'
   get 'offer_target_players/index/:offer_target_id', to: 'offer_target_players#index', as: :offer_target_players
   resources :offer_targets
   resources :offers
   resources :players
 
-  root "offer_targets#index"
+  # root "offer_targets#index"
+  root "components#index"
 end
