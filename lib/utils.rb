c9de8e53324b8f1241edbf117213392462fd7d23
Brian Olpin <bolpin@gmail.com>
2022-03-29 17:13:41 -0700

    change name of parse_version util function

--- lib/utils.rb
+++ lib/utils.rb
@@ -2,9 +2,9 @@ require 'rubygems/version'
 
 module Utils
 
-  # returns array of minimum 3 segments (maj, minor, patch)
+  # Returns array of three segments (maj, minor, patch)
   # NOTE: pre-release suffixes are ignored
-  def parse_version(version_str)
+  def parse_vers(version_str)
     segments = Gem::Version.new(version_str).release.segments
     while segments.length < 3
       segments.push 0
@@ -12,9 +12,5 @@ module Utils
     segments
   end
 
-  # def birthdate_range_for(min_age, max_age=150)
-  #   (max_age.years.ago.yesterday.to_date..min_age.years.ago.to_date)
-  # end
-  #
 end
 
